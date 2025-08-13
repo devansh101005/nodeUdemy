c09344b92c27cc454c8161b4eff2d035f9022ee8
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-08-14 01:01:16 +0530

    Leared datatypes

---
+++ datatype.js
@@ -0,0 +1,30 @@
+// ,/* */ these twoo ways are of commenting 
+
+/* 
+String
+Number
+Boolan
+Bigint
+
+Undefined --no definition for it(if fulfilled then hogi)
+null-- its empty (not 0)
+
+Object 
+
+Symbol--make thing unique like unique value unique subject etc ...
+*/
+
+var score =102  //var is probamatic //var can be scored anyway in the code 
+//so we use let because of scope 
+let score =102;
+
+let name ="Chai aur code "
+let isLoggedIn=true
+
+//object ka example
+let teaType=["lemon tea","Orange tea ","idk kaiun si tea"]
+
+let user={firstNmae:"Devansh",lastName:"Choudhary"}
+
+let getScore =score //isse hamne isi variable mai upar wale ki value leli
+ console.log(getScore)
---
+++ printing.js
@@ -0,0 +1,7 @@
+console.log("Hello")//ist way of printing 
+
+process.stdout.write("chai") //2nd way but priunts in same line 
+console.table("test")
+
+console.table({city:"jaipur"})
+
