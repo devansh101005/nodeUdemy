d58119620f20b9f5dfa98510789a2ff55f88dcbf
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-08-14 10:13:03 +0530

    morning push

---
+++ part3/condition-challenges.js
@@ -0,0 +1,81 @@
+// check which number is greater 
+let num1=5
+let num2=8
+
+if (num1>num2) {
+console.log("num 1 is greater than num2")
+} else {
+    console.log("nope num1 isnt")
+}
+
+console.log("i am regular bottom code ")
+
+
+//Ques 2 //check if ywo syting are equal 
+
+let username="chai"
+let anotherUsername ="chai"
+
+
+if (username==anotherUsername) {
+    console.log("pick another username ")
+} else {
+    console.log.og("you can ppick thisnusername ")
+}
+
+//checking if a variable is number or not
+
+let score =44
+if(typeof score==='number') {
+console.log("yep it,s a number ")
+} else {
+    console.log("this isnit a number ")
+}
+
+//Checkif a boolean number is true or false 
+
+let isReady =false 
+
+if (isReady)  {
+    console.log("yes it is ")
+}  else {
+    console.log("no it is not boht acha hua i hate tea")
+}
+
+//check if array is empty or not
+
+let items =[]
+
+if (items.length===0) {
+console.log("array is empty")
+} else {
+    console.log("its not")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
