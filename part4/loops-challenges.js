464736980cdecb1cb53157121df13125e73dd05c
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-08-18 02:11:16 +0530

    node

---
+++ part4/loops-challenges.js
@@ -0,0 +1,10 @@
+/*
+Write a while loop that calculates the sum of all numbers from 1 to 5 qnd stores in a varible */
+
+let sum=0;
+let i=0;
+while(i<=5) {
+    sum=sum+i
+    i++
+}
+console.log(sum)
